9ddb162674782d7409c68af598556bdab902c7ff
RamidAhadov <ramidehedov02@gmail.com>
2024-04-22 07:18:02 +0400

    Const error messages added

---
+++ FileShare.Business/Constants/ErrorMessages.cs
@@ -0,0 +1,9 @@
+namespace FileShare.Business.Constants;
+
+public static class ErrorMessages
+{
+    public const string LocalIPNotFound = "Local IP address not found.";
+    public const string DNSInnerExceptionMessage = "1. Please make sure you connected to the network.\n " +
+                                                   "2. Try to restart your connection. \n " +
+                                                   "3. Check your network firewall settings.";
+}
