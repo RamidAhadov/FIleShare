237137fc48766453d16de29d30eb8037018a19b5
RamidAhadov <ramidehedov02@gmail.com>
2024-04-22 07:18:59 +0400

    Get LocalIPAddress and adjust IPs

--- FileShare.Business/DeviceManager.cs
+++ FileShare.Business/DeviceManager.cs
@@ -1,13 +1,31 @@
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using FileShare.Business.Abstraction;
+using FileShare.Business.Constants;
 
 namespace FileShare.Business;
 
 public class DeviceManager:IDeviceManager
 {
-    public List<string> GetLocalDeviceIPs()
+    public List<string> GetLocalDeviceIPs(string? subnetMask = default)
     {
-        throw new NotImplementedException();
+        string IP = GetLocalIPAddress();
+        int[] loopCounts = LoopCounts(subnetMask);
+        for (int i = 0; i < loopCounts[0]; i++)
+        {
+            string subIP = IP[..IP.LastIndexOf('.')];
+            string lastOcta = subIP[(subIP.LastIndexOf('.') + 1)..];
+            lastOcta = (int.Parse(lastOcta) + i).ToString();
+            subIP = subIP[..(subIP.LastIndexOf('.') + 1)];
+            subIP += lastOcta + ".";
+            for (int j = 1; j < loopCounts[1]; j++)
+            {
+                string fullIP = subIP + j;
+            }
+        }
+        
+        return new List<string>();
     }
 
     public string? GetSubnetMask()
@@ -25,16 +43,55 @@ public class DeviceManager:IDeviceManager
                 {
                     foreach (UnicastIPAddressInformation ip in properties.UnicastAddresses)
                     {
-                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
                         {
                             return ip.IPv4Mask.ToString();
                         }
                     }
                 }
             }
-
-            return null;
         }
+        
         throw new NetworkInformationException();
+    }
+    
+    static string GetLocalIPAddress()
+    {
+        NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+        
+        foreach (NetworkInterface networkInterface in networkInterfaces)
+        {
+            if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && networkInterface.OperationalStatus == OperationalStatus.Up)
+            {
+                var ipProperties = networkInterface.GetIPProperties();
+                var ipAddress = ipProperties.UnicastAddresses.FirstOrDefault(x => x.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+                if (ipAddress != null)
+                {
+                    return ipAddress.Address.ToString();
+                }
+            }
+        }
+
+        throw new Exception($"{ErrorMessages.LocalIPNotFound} \n {ErrorMessages.DNSInnerExceptionMessage}");
+    }
+
+    private int[] LoopCounts(string? subnetMask)
+    {
+        int[] loopCounts = new int[2];
+        if (subnetMask == null)
+        {
+            loopCounts[0] = 1;
+            loopCounts[1] = 256;
+        }
+        else
+        {
+            string[] subnetMaskSectors = subnetMask.Split('.');
+            for (int i = 2; i < subnetMaskSectors.Length; i++)
+            {
+                loopCounts[i - 2] = 256 - int.Parse(subnetMaskSectors[i]);
+            }
+        }
+
+        return loopCounts;
     }
 }
