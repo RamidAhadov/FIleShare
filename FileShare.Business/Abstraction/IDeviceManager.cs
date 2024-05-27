4681fe3a63f3578abe5d09cba39c8bf6d0b0ad92
ramidahadov <ramidehedov02@gmail.com>
2024-05-28 00:28:41 +0400

    IsOnline check

--- FileShare.Business/Abstraction/IDeviceManager.cs
+++ FileShare.Business/Abstraction/IDeviceManager.cs
@@ -5,4 +5,5 @@ public interface IDeviceManager
     List<string> GetLocalDeviceIPs(string? subnetMask = default, int timeOut = 500);
     IAsyncEnumerable<string> GetLocalDeviceIPsAsync(string? subnetMask = default, int timeOut = 500);
     string? GetSubnetMask();
+    Task<bool> IsOnline(string ip);
 }
