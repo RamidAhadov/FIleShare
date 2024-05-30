323be61cd0c2b4df26201761bb974bf94202319a
ramidahadov <ramidehedov02@gmail.com>
2024-05-30 13:09:52 +0400

    New Request and receive methods

--- FileShare.Business/Abstraction/INotifyManager.cs
+++ FileShare.Business/Abstraction/INotifyManager.cs
@@ -6,8 +6,11 @@ namespace FileShare.Business.Abstraction;
 public interface INotifyManager
 {
     Task<Result<TcpListener>> CreateListenerAsync();
+    Task<Result<TcpListener>> CreateRequestReceiverAsync();
+    Task<Result<TcpListener>> CreateResponseReceiverAsync();
     Task<Result> SendRequestAsync(string destinationIp, int port);
-    Task<Result> SendResponseAsync(bool response);
+    Task<Result> SendResponseAsync(bool response, string destinationIp, int port);
     IAsyncEnumerable<string> GetReceivedFilenameAsync();
+    IAsyncEnumerable<string?> GetRequestAsync();
     Task<Result> SendFilenameAsync(string destinationIp, int port, string filename);
 }
--- FileShare.Business/Concrete/TcpNotifyManager.cs
+++ FileShare.Business/Concrete/TcpNotifyManager.cs
@@ -15,37 +15,37 @@ namespace FileShare.Business.Concrete;
 public class TcpNotifyManager:INotifyManager
 {
     private static TcpListener? _tcpListener;
-    private static TcpListener? _requestReceiver;
+    private static TcpListener? _tcpRequestReceiver;
+    private static TcpListener? _tcpResponseReceiver;
     private static List<Socket> _sockets;
+    private static List<Socket> _requesterSockets;
     private static string _myIp;
     
     private readonly TcpListenerParameters _tcpListenerParameters;
+    private readonly TcpRequestReceiverParameters _tcpRequestReceiverParameters;
+    private readonly TcpResponseReceiverParameters _tcpResponseReceiverParameters;
     private readonly Logger _logger;
 
     public TcpNotifyManager(IConfigFactory configFactory)
     {
-        _tcpListenerParameters = (TcpListenerParameters)configFactory.GetConfiguration("TcpListenerParameters");
         _sockets = new List<Socket>();
+        _requesterSockets = new List<Socket>();
         _myIp = GetCurrentIPv4Address();
+        
+        _tcpListenerParameters = (TcpListenerParameters)configFactory.GetConfiguration("TcpListenerParameters");
+        _tcpRequestReceiverParameters = (TcpRequestReceiverParameters)configFactory.GetConfiguration("TcpRequestReceiverParameters");
+        _tcpResponseReceiverParameters = (TcpResponseReceiverParameters)configFactory.GetConfiguration("TcpResponseReceiverParameters");
         _logger = LogManager.GetLogger("NotifyManagerLogger");
     }
 
+    #region Create TCP listener
     public async Task<Result<TcpListener>> CreateListenerAsync()
     {
         var sw = Stopwatch.StartNew();
-        var ipAddress = IPAddress.Parse(_tcpListenerParameters.Host);
         try
         {
-            await Task.Run(() =>
-            {
-                if (_tcpListener == null)
-                {
-                    _tcpListener = new TcpListener(ipAddress, _tcpListenerParameters.Port);
-                    _tcpListener.Start();
-                
-                    _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(CreateListenerAsync), Message = $"Tcp listener started and listening on: {_tcpListenerParameters.Host}:{_tcpListenerParameters.Port}"}.ToJson());
-                }
-            });
+            _tcpListener = await StartTcpListenerAsync(_tcpListener, _tcpListenerParameters.Host, _tcpListenerParameters.Port);
+            _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(CreateListenerAsync), Message = $"Tcp listener started and listening on: {_tcpListenerParameters.Host}:{_tcpListenerParameters.Port}"}.ToJson());
             
             return Result.Ok(_tcpListener);
         }
@@ -57,6 +57,47 @@ public class TcpNotifyManager:INotifyManager
         }
     }
 
+    public async Task<Result<TcpListener>> CreateRequestReceiverAsync()
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            _tcpRequestReceiver = await StartTcpListenerAsync(_tcpRequestReceiver, IPAddress.Any,
+                _tcpRequestReceiverParameters.Port);
+            _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(CreateRequestReceiverAsync), Message = $"Tcp request receiver started and listening on: {_tcpRequestReceiverParameters.Host}:{_tcpRequestReceiverParameters.Port}"}.ToJson());
+            
+            return Result.Ok(_tcpRequestReceiver);
+        }
+        catch (Exception e)
+        {
+            _logger.Error(new {Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(CreateRequestReceiverAsync), Message = e.InnerException?.Message ?? e.Message}.ToJson());
+            
+            return Result.Fail(e.InnerException?.Message ?? e.Message);
+        }
+    }
+
+    public async Task<Result<TcpListener>> CreateResponseReceiverAsync()
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            _tcpResponseReceiver = await StartTcpListenerAsync(_tcpResponseReceiver, IPAddress.Any,
+                _tcpResponseReceiverParameters.Port);
+            _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(CreateResponseReceiverAsync), Message = $"Tcp response receiver started and listening on: {_tcpResponseReceiverParameters.Host}:{_tcpResponseReceiverParameters.Port}"}.ToJson());
+            
+            return Result.Ok(_tcpResponseReceiver);
+        }
+        catch (Exception e)
+        {
+            _logger.Error(new {Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(CreateResponseReceiverAsync), Message = e.InnerException?.Message ?? e.Message}.ToJson());
+            
+            return Result.Fail(e.InnerException?.Message ?? e.Message);
+        }
+    }
+
+    #endregion
+    
+    #region Send request to receiver
     public async Task<Result> SendRequestAsync(string destinationIp, int port)
     {
         var sw = Stopwatch.StartNew();
@@ -67,26 +108,42 @@ public class TcpNotifyManager:INotifyManager
                 await client.ConnectAsync(destinationIp, port);
                 _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request sent to {destinationIp}"}.ToJson());
 
-                using (var stream = client.GetStream())
+                var socket = await _tcpResponseReceiver.AcceptSocketAsync();
+                var response = await GetMessage(socket);
+                while (!IsResponse(response))
                 {
-                    var response = await ReadBufferAsync(stream);
-
-                    while (!IsResponse(response))
-                    {
-                        response = await ReadBufferAsync(stream);
-                    }
-
-                    if (ResponseResult(response))
-                    {
-                        _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request accepted by {destinationIp}."}.ToJson());
-                        
-                        return Result.Ok();
-                    }
-                    
-                    _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request not accepted by {destinationIp}."}.ToJson());
-                    
-                    return Result.Fail("Receiver not accepted the file.");
+                    response = await GetMessage(socket);
                 }
+                if (ResponseResult(response))
+                {
+                    _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request accepted by {destinationIp}."}.ToJson());
+                        
+                    return Result.Ok();
+                }
+                    
+                _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request not accepted by {destinationIp}."}.ToJson());
+                    
+                return Result.Fail("Receiver not accepted the file.");
+                //using (var stream = client.GetStream())
+                //{
+                //    var response = await ReadBufferAsync(stream);
+//
+                //    while (!IsResponse(response))
+                //    {
+                //        response = await ReadBufferAsync(stream);
+                //    }
+//
+                //    if (ResponseResult(response))
+                //    {
+                //        _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request accepted by {destinationIp}."}.ToJson());
+                //        
+                //        return Result.Ok();
+                //    }
+                //    
+                //    _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendRequestAsync), Message = $"Request not accepted by {destinationIp}."}.ToJson());
+                //    
+                //    return Result.Fail("Receiver not accepted the file.");
+                //}
             }
         }
         catch (Exception e)
@@ -97,16 +154,41 @@ public class TcpNotifyManager:INotifyManager
         }
     }
 
-    public Task<Result> SendResponseAsync(bool response)
+    #endregion
+
+    public async Task<Result> SendResponseAsync(bool response, string destinationIp, int port)
     {
-        throw new NotImplementedException();
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            await SendResponseAsync(destinationIp, port, response.ToString());
+            Console.WriteLine($"Response sent to: {destinationIp}. Response {response}"); //Will be removed
+            _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendResponseAsync), Message = $"Response sent to {destinationIp}", Response = $"{response}"}.ToJson());
+            
+            return Result.Ok();
+        }
+        catch (Exception e)
+        {
+            _logger.Error(new {Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(SendResponseAsync), Message = e.InnerException?.Message ?? e.Message}.ToJson());
+            
+            return Result.Fail(e.InnerException?.Message ?? e.Message);
+        }
     }
 
     public async IAsyncEnumerable<string> GetReceivedFilenameAsync()
     {
-        await foreach (var client in this.AcceptClientsAsync())
+        await foreach (var client in this.AcceptClientsAsync(_tcpListener))
         {
             yield return await GetMessage(client);
+        }
+    }
+
+    public async IAsyncEnumerable<string?> GetRequestAsync()
+    {
+        await foreach (var client in this.AcceptClientsAsync(_tcpRequestReceiver))
+        {
+            Console.WriteLine("Request received from: "+ GetRemoteIpFromSocket(client)); //Will be removed
+            yield return GetRemoteIpFromSocket(client);
         }
     }
 
@@ -133,16 +215,16 @@ public class TcpNotifyManager:INotifyManager
     }
 
 
-    private async IAsyncEnumerable<Socket> AcceptClientsAsync()
+    private async IAsyncEnumerable<Socket> AcceptClientsAsync(TcpListener? listener)
     {
         while (true)
         {
             var sw = Stopwatch.StartNew();
-            if (_tcpListener != null)
+            if (listener != null)
             {
-                var socket = await _tcpListener.AcceptSocketAsync();
+                var socket = await listener.AcceptSocketAsync();
                 _logger.Info(new { Elapsed = $"{sw.ElapsedMilliseconds} ms", Method = nameof(AcceptClientsAsync), Message = "New socket accepted."}.ToJson());
-                AddClient(socket);
+                AddClient(socket, _sockets);
                 
                 yield return socket;
             }
@@ -159,8 +241,8 @@ public class TcpNotifyManager:INotifyManager
         {
             var message = await Task.Run(() => ReceiveMessages(client));
             message = message + "-" + endPointIP;
-            await client.DisconnectAsync(true);
-            RemoveClient(client);
+            await client.DisconnectAsync(true); //TODO
+            RemoveClient(client, _sockets);
 
             return message;
         }
@@ -181,9 +263,9 @@ public class TcpNotifyManager:INotifyManager
                 string result = Encoding.UTF8.GetString(data, 0, bytesRead);
                 return result;
             }
-
             await client.DisconnectAsync(false);
-            return "null";
+            
+            return string.Empty;
         }
         catch (Exception e)
         {
@@ -197,14 +279,14 @@ public class TcpNotifyManager:INotifyManager
         return ((IPEndPoint)client.RemoteEndPoint).Address.ToString();
     }
 
-    private void AddClient(Socket socket)
+    private void AddClient(Socket socket, List<Socket> sockets)
     {
-        _sockets.Add(socket);
+        sockets.Add(socket);
     }
 
-    private void RemoveClient(Socket socket)
+    private void RemoveClient(Socket socket, List<Socket> sockets)
     {
-        _sockets.Remove(socket);
+        sockets.Remove(socket);
     }
 
     private bool IsResponse(string stream)
@@ -253,5 +335,58 @@ public class TcpNotifyManager:INotifyManager
         }
 
         throw new NetworkInformationException();
+    }
+
+    private async Task<TcpListener?> StartTcpListenerAsync(TcpListener? listener,string host, int port)
+    {
+        var ipAddress = IPAddress.Parse(host);
+        await Task.Run(() => 
+        {
+            if (listener == null)
+            {
+                listener = new TcpListener(ipAddress,  port);
+                listener.Start();
+            }
+        });
+
+        return listener;
+    }
+    private async Task<TcpListener?> StartTcpListenerAsync(TcpListener? listener,IPAddress ipAddress, int port)
+    {
+        await Task.Run(() => 
+        {
+            if (listener == null)
+            {
+                listener = new TcpListener(ipAddress,  port);
+                listener.Start();
+            }
+        });
+
+        return listener;
+    }
+
+    private string? GetSenderIpFromMessage(string message)
+    {
+        var dashIndex = message.LastIndexOf('-');
+        return message[(dashIndex + 1)..];
+    }
+
+    private string? GetRemoteIpFromSocket(Socket socket)
+    {
+        if (socket.RemoteEndPoint is IPEndPoint remoteEndPoint)
+        {
+            return remoteEndPoint.Address.ToString();
+        }
+
+        return null;
+    }
+
+    private async Task SendResponseAsync(string destinationIp, int port, string response)
+    {
+        var tcpClient = new TcpClient(destinationIp, port);
+        var stream = tcpClient.GetStream();
+        var data = Encoding.UTF8.GetBytes(response.ToString());
+        await stream.WriteAsync(data, 0, data.Length);
+        await stream.FlushAsync();
     }
 }
