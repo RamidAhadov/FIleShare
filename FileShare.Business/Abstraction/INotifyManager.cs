23e3db2c654dd0d05f161cf1ff9ef3eca795b953
ramidahadov <ramidehedov02@gmail.com>
2024-05-31 21:19:01 +0400

    Response parameter switched to enum

--- FileShare.Business/Abstraction/INotifyManager.cs
+++ FileShare.Business/Abstraction/INotifyManager.cs
@@ -1,4 +1,5 @@
 using System.Net.Sockets;
+using FileShare.Business.Constants;
 using FileShare.Business.Models;
 using FluentResults;
 
@@ -10,7 +11,7 @@ public interface INotifyManager
     Task<Result<TcpListener>> CreateRequestReceiverAsync(CancellationToken token);
     Task<Result<TcpListener>> CreateResponseReceiverAsync(CancellationToken token);
     Task<Result> SendRequestAsync(string destinationIp, int port, CancellationToken token);
-    Task<Result> SendResponseAsync(bool response, string destinationIp, int port, CancellationToken token);
+    Task<Result> SendResponseAsync(Responses response, string destinationIp, int port, CancellationToken token);
     IAsyncEnumerable<MessageModel?> GetReceivedFilenameAsync(CancellationToken token);
     IAsyncEnumerable<string?> GetRequestAsync(CancellationToken token);
     Task<Result> SendFilenameAsync(string destinationIp, int port, string filename, CancellationToken token);
