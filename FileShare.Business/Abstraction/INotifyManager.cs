bccab20fff1b1bc4aef2e9d5dd3422e1beed4f2c
ramidahadov <ramidehedov02@gmail.com>
2024-05-31 19:35:04 +0400

    Signature change

--- FileShare.Business/Abstraction/INotifyManager.cs
+++ FileShare.Business/Abstraction/INotifyManager.cs
@@ -1,16 +1,17 @@
 using System.Net.Sockets;
+using FileShare.Business.Models;
 using FluentResults;
 
 namespace FileShare.Business.Abstraction;
 
 public interface INotifyManager
 {
-    Task<Result<TcpListener>> CreateListenerAsync();
-    Task<Result<TcpListener>> CreateRequestReceiverAsync();
-    Task<Result<TcpListener>> CreateResponseReceiverAsync();
-    Task<Result> SendRequestAsync(string destinationIp, int port);
-    Task<Result> SendResponseAsync(bool response, string destinationIp, int port);
-    IAsyncEnumerable<string> GetReceivedFilenameAsync();
-    IAsyncEnumerable<string?> GetRequestAsync();
-    Task<Result> SendFilenameAsync(string destinationIp, int port, string filename);
+    Task<Result<TcpListener>> CreateListenerAsync(CancellationToken token);
+    Task<Result<TcpListener>> CreateRequestReceiverAsync(CancellationToken token);
+    Task<Result<TcpListener>> CreateResponseReceiverAsync(CancellationToken token);
+    Task<Result> SendRequestAsync(string destinationIp, int port, CancellationToken token);
+    Task<Result> SendResponseAsync(bool response, string destinationIp, int port, CancellationToken token);
+    IAsyncEnumerable<MessageModel?> GetReceivedFilenameAsync(CancellationToken token);
+    IAsyncEnumerable<string?> GetRequestAsync(CancellationToken token);
+    Task<Result> SendFilenameAsync(string destinationIp, int port, string filename, CancellationToken token);
 }
