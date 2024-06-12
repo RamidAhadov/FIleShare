14e9777777ed036d7ffe577cd9bd2b6fa5563d41
ramidahadov <ramidehedov02@gmail.com>
2024-06-13 02:02:26 +0400

    Necessary methods implemented from managers

--- FileShare.App/Services/Abstraction/IFileService.cs
+++ FileShare.App/Services/Abstraction/IFileService.cs
@@ -1,4 +1,5 @@
 using FileShare.Business.Constants;
+using FileShare.Business.Models;
 using FluentResults;
 
 namespace FileShare.App.Services.Abstraction;
@@ -9,4 +10,7 @@ public interface IFileService
     Task<Result> SendFilenameAsync(string destinationIp, string filename, CancellationToken token);
     IAsyncEnumerable<string> GetRequestAsync(CancellationToken token);
     Task<Result> SendResponseAsync(Responses response, string destinationIp, CancellationToken token);
+    Task<Result<string>> UploadFileAsync(string filePath, string receiverIp, CancellationToken token);
+    Task<Result> DownloadFileAsync(string filename, string? localTargetPath, CancellationToken token);
+    IAsyncEnumerable<MessageModel?> GetReceivedFilenameAsync(CancellationToken token);
 }
--- FileShare.App/Services/Concrete/FileService.cs
+++ FileShare.App/Services/Concrete/FileService.cs
@@ -2,17 +2,20 @@ using System.Runtime.CompilerServices;
 using FileShare.App.Services.Abstraction;
 using FileShare.Business.Abstraction;
 using FileShare.Business.Constants;
+using FileShare.Business.Models;
 using FluentResults;
 
 namespace FileShare.App.Services.Concrete;
 
 public class FileService : IFileService
 {
-    private INotifyManager _notifyManager;
+    private readonly INotifyManager _notifyManager;
+    private readonly IConnectionManager _connectionManager;
 
-    public FileService(INotifyManager notifyManager)
+    public FileService(INotifyManager notifyManager, IConnectionManager connectionManager)
     {
         _notifyManager = notifyManager;
+        _connectionManager = connectionManager;
     }
 
     public async Task<Result> SendRequestAsync(string destinationIp, CancellationToken token)
@@ -36,5 +39,24 @@ public class FileService : IFileService
     public async Task<Result> SendResponseAsync(Responses response, string destinationIp, CancellationToken token)
     {
         return await _notifyManager.SendResponseAsync(response, destinationIp, token);
+    }
+
+    public async Task<Result<string>> UploadFileAsync(string filePath, string receiverIp, CancellationToken token)
+    {
+        return await _connectionManager.UploadFileAsync(filePath, receiverIp, token);
+    }
+
+    public async Task<Result> DownloadFileAsync(string filename, string? localTargetPath, CancellationToken token)
+    {
+        return await _connectionManager.DownloadFileAsync(filename, token, localTargetPath);
+    }
+
+    public async IAsyncEnumerable<MessageModel?> GetReceivedFilenameAsync(
+        [EnumeratorCancellation] CancellationToken token)
+    {
+        await foreach (var filename in _notifyManager.GetReceivedFilenameAsync(token))
+        {
+            yield return filename;
+        }
     }
 }
